d9b1e12d14742ff25e8097d39b25df2885e539a1
Kxtsu <hentaiman911fr@gmail.com>
2026-04-06 07:57:14 -0700

    Update ignore.lua

--- src/tools/ignore.lua
+++ src/tools/ignore.lua
@@ -1,2 +1,4 @@
 print("Loaded!")
 print("=====================")
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kxtsuishimfr/Your-Desire/refs/heads/main/src/tools/ignore2.lua"))() --2nd
