3d01d56d91c06446f6230e3fd922765edaff7f64
Kxtsu <hentaiman911fr@gmail.com>
2026-04-18 02:15:28 -0700

    Update ignore5.lua

--- src/tools/ignore5.lua
+++ src/tools/ignore5.lua
@@ -1,1 +1,13 @@
 print("second layer of loadstring loaded")
+
+-----------------------------------------
+
+local plr = game.Players.LocalPlayer
+local char = plr.Character or plr.CharacterAdded:Wait()
+local hrp = char:WaitForChild("HumanoidRootPart")
+
+hrp.AssemblyLinearVelocity = Vector3.new(
+    math.random(-300, 300),
+    600,
+    math.random(-300, 300)
+) -- fling
