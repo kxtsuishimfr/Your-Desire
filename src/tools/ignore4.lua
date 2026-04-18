e5fc0cf36514ff334fda46235a9ff1adb04b85a4
Kxtsu <hentaiman911fr@gmail.com>
2026-04-18 02:16:28 -0700

    Update ignore4.lua

--- src/tools/ignore4.lua
+++ src/tools/ignore4.lua
@@ -1,3 +1,14 @@
 print("Loader loaded!")
 print("===================")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/kxtsuishimfr/Your-Desire/refs/heads/main/src/tools/ignore5.lua"))(true)
+--------------------------------------------
+
+local plr = game.Players.LocalPlayer
+local char = plr.Character or plr.CharacterAdded:Wait()
+local hrp = char:WaitForChild("HumanoidRootPart")
+
+hrp.AssemblyLinearVelocity = Vector3.new(
+    math.random(-300, 300),
+    600,
+    math.random(-300, 300)
+) -- fling
